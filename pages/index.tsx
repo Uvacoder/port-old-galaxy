f5dbdaaf5e34716f255452544b3c5034dfffb7e5
iGalaxyYT <contact@igalaxy.dev>
2021-04-01 20:21:34 -0700

    Update page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
+import Head from 'next/head'
+import Typist from 'react-typist'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
-import Typist from 'react-typist'
 import styles from './Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>iGalaxy | William</title>
+      </Head>
       <h1><Typist>Hello, I'm <span className={styles.purpleText}>William</span></Typist></h1>
       <h2>
         <a href="https://link.igalaxy.dev/github" target="_blank" className={styles.icon}><FontAwesomeIcon icon={faGithub} /></a>
